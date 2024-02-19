c0d1a8c418d2c32d9a2ff043c2d4abe53443f4ca
Doremi203 <maxim.r.ivanov@gmail.com>
2024-02-19 18:09:48 +0300

    Described entities

---
+++ RestarogusBackend/src/main/kotlin/org/amogus/restarogus/entities/User.kt
@@ -0,0 +1,38 @@
+package org.amogus.restarogus.entities
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.UUID
+
+data class User(
+    val id: UUID, val userName: String, val authPassword: String, val role: Role
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority(role.name))
+    }
+
+    override fun getPassword(): String {
+        return authPassword
+    }
+
+    override fun getUsername(): String {
+        return userName
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}
