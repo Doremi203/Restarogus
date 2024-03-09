f490a4c8d63294394509c5de4b6205eb45ef7fdf
Doremi203 <maxim.r.ivanov@gmail.com>
2024-03-10 02:32:06 +0300

    - renamed strategy

--- RestarogusBackend/src/main/kotlin/org/amogus/restarogus/services/orderSystem/FifoOrderPriorityStrategy.kt
+++ RestarogusBackend/src/main/kotlin/org/amogus/restarogus/services/orderSystem/FifoOrderPriorityStrategy.kt
@@ -3,7 +3,7 @@ package org.amogus.restarogus.services.orderSystem
 import org.amogus.restarogus.models.Order
 import org.amogus.restarogus.services.interfaces.orderSystem.OrderPriorityStrategy
 
-class OlderOrdersFirstPriorityStrategy : OrderPriorityStrategy {
+class FifoOrderPriorityStrategy : OrderPriorityStrategy {
     override fun getPriorityOrders(orders: List<Order>): List<Order> {
         return orders.sortedBy { it.date }
     }
